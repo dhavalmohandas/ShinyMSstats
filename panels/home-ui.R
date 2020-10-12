56d25d1b994b4b9ec28638ee5404199ca1bba9e4
Dhaval Mohandas <dhavalmohandas@gmail.com>
2020-10-12 18:07:11 -0400

    Change github link.

--- panels/home-ui.R
+++ panels/home-ui.R
@@ -8,7 +8,7 @@ home = fluidPage(
       h4("About MSstats-Shiny"),
       p("This is a web tool for the statistical analysis of quantitative proteomic data.  It is built based on the R package ", a("MSstats (v 3.10.2).", href="http://msstats.org/")),
       p("PLease note that some calculations may take some time to compute."),
-      p("The full code can be accessed online at ", a("https://github.com/cristinapasi/ShinyMSstats.", href="https://github.com/cristinapasi/ShinyMSstats")),
+      p("The full code can be accessed online at ", a("here.", href="https://github.com/dhavalmohandas/ShinyMSstats")),
       br(),
       br()
 #      actionButton("Foo", "Fooooo")
